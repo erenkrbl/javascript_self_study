8ab96456e196a2f0d73bc97dba5420234e0661e8
Joseph Abraham <ekurubal24@gmail.com>
2021-03-11 20:45:04 +0300

    Added number

--- data_type.js
+++ data_type.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+
+let number = 50;
+console.log(number);
+console.log(typeof number);
+
+let floatNumber = 25.452;
+console.log(floatNumber);
+console.log(typeof floatNumber);
+
+let negativeNumber = -20;
+console.log(negativeNumber);
+console.log(typeof negativeNumber);
