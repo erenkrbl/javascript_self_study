7fb3882aedbede66708c5303612552ade07183a0
Joseph Abraham <ekurubal24@gmail.com>
2021-03-11 21:03:45 +0300

    Added string data type, escape expressions

--- data_type.js
+++ data_type.js
@@ -1,5 +1,7 @@
 console.log("Hello World");
 
+// type number
+
 let number = 50;
 console.log(number);
 console.log(typeof number);
@@ -11,3 +13,24 @@ console.log(typeof floatNumber);
 let negativeNumber = -20;
 console.log(negativeNumber);
 console.log(typeof negativeNumber);
+
+// type string
+
+let firstName = "Arthur";
+console.log(firstName);
+console.log(typeof firstName);
+
+let lastName = 'King';
+console.log(lastName);
+console.log(typeof lastName);
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+let statement = "Arthur's computer";
+console.log(statement);
+
+ // escape expressions
+let statement1 = 'Arthur\'s computer';
+console.log(statement1);
+
