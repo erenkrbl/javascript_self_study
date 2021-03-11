93acf3905bf1a056203d8fa8ad00b6409594aa01
Joseph Abraham <ekurubal24@gmail.com>
2021-03-11 23:00:25 +0300

    Added arithmetic, inc-dec, comparison, logical

---
+++ operators.js
@@ -0,0 +1,75 @@
+// Opreators
+// Arithmetic operators + - * / %
+
+// let number1 = 10;
+// let number2 = 3;
+
+// //addition
+
+// let result = number1 + number2;
+// // subtraction
+// result = number1 - number2;
+// // multiply
+// result = number1 * number2;
+// // Division
+// result = number1 / number2;
+
+// //moduls
+// result = number1 % number2;
+// console.log(result);
+
+// Increment-decrement opetators
+// number1 = number1 + 1;
+// number1++;
+// ++number1;
+// number1--;
+// --number1;
+// console.log(number1);
+
+// console.log("Number 2 : " + number2);
+// console.log("Number 2 : " + number2++);
+// console.log("Number 2 : " + number2);
+
+// let number3 = 20;
+// console.log(++number3);
+// console.log(number3);
+// console.log(number3++);
+
+// Arithmetic assignment operators
+// +=, -=, *=, /=, %=
+
+let number1 = 10;
+let number2 = 9;
+
+//number2 += number1; // number2 = number2 + number1;
+//number2 -= number1; // number2 = number2- number1;
+//number2 *= number1; // number2 = number2 * number1;
+//number2 /= number1; // number2 = number2 / number1;
+
+console.log(number2);
+
+//comparison operators
+
+console.log(number1 > number2);
+console.log(number1 < number2);
+console.log(number1 >= number2);
+console.log(number1 <= number2);
+console.log(number1 == number2);
+console.log(number1 != number2);
+
+//relational operators
+
+let age = 30;
+let ageNew = "30";
+console.log(age == ageNew); // value comparison
+console.log(age === ageNew); // value and type comparison
+console.log(age !== ageNew); // value and type comparison
+
+// logical operators and, or
+
+console.log((number1 < 5) && ( number2 < 10));
+console.log((number1 > 5) && ( number2 < 10));
+console.log((number1 > 5) || ( number2 < 10));
+console.log((number1 < 5) || ( number2 > 10));
+
+
