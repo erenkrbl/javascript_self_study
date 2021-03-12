4f83f2099a03ea6a313bebb4df4d675ff5d24a20
Joseph Abraham <ekurubal24@gmail.com>
2021-03-12 22:40:35 +0300

    Account template

---
+++ celcius.js
@@ -0,0 +1,6 @@
+// Fahrenheit in celcius account
+
+let cFahrenheit = parseInt(prompt("Enter fahrenheit", "100"));
+let cCelcius = (5 / 9) * (cFahrenheit - 32);
+
+console.log(`You enterd ${cFahrenheit} fahrenheit temperature ${cCelcius} degree.`);
