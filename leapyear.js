f0046c20b69e12188667b26a15ee8f5eccce6278
Joseph Abraham <ekurubal24@gmail.com>
2021-03-12 22:59:37 +0300

    Added leapyear count with function

--- leapyear.js
+++ leapyear.js
@@ -1,11 +1,15 @@
 // leap year account
 
-let year = parseInt(prompt("Enter a year", "2020"));
-let leapyear =(year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0);
+// let year = parseInt(prompt("Enter a year", "2020"));
+// let leapyear =(year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0);
 
-console.log(year + " leapyear ? : " + leapyear);
-
-
+// console.log(year + " leapyear ? : " + leapyear);
 
 
+
+function leapyear(year){
+    return (year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0);
+};
+
+console.log(leapyear(4100));
 
