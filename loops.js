64fc91c226f73b0153c6be9ee4178fbfcf707bbb
Joseph Abraham <ekurubal24@gmail.com>
2021-03-14 14:05:32 +0300

    added for loops

---
+++ loops.js
@@ -0,0 +1,10 @@
+// for loops
+// for (ilk atama; döngü çalıştırma şartı; her çalışma sonrası olacak 
+
+for ( let i = 0; i < 5; i++) {
+    console.log("My name is Eren");
+}
+
+for (let i = 0, j = 0; (i + j) < 20; i = i + 5, j = j + 2) {
+    console.log("i value : " + i + "," + " j value : " + j)
+}
