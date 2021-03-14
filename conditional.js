0fa6db0805fbe42d602eaf3383140c1f90ef1b34
Joseph Abraham <ekurubal24@gmail.com>
2021-03-14 11:55:18 +0300

    Added if and else simple to use

---
+++ conditional.js
@@ -0,0 +1,10 @@
+
+let myAge = 35, yourAge = 40;
+
+if (myAge < yourAge) {
+    console.log("I am smaller than you");
+} else if (myAge == yourAge) {
+    console.log("We are the same age");
+} else {
+    console.log("I am bigger than you");
+} 
