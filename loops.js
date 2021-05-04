94904ff9dfbc6b7dfa9f9eb1de830dfa06153558
Joseph Abraham <ekurubal24@gmail.com>
2021-05-04 15:59:17 +0300

    Added using for string with string

--- loops.js
+++ loops.js
@@ -75,3 +75,15 @@ for(let i = 1; i <= 5; i++) {
 console.log("multiply: ", multiply);
 
 
+let myString = 'Clarusway';
+let result = "";
+
+for (i= 0; i < myString.length; i++) {
+    console.log(` ${i+1}.ci harf ${myString[i]}`);
+    for(let j = 0; j < 2; j++) {
+        result += myString[i] 
+    }
+}
+console.log(result);
+
+
